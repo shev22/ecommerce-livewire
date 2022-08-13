40f31da3a1753a907bdf40863ab37f98c75bf60a
Francis <shevy2k2@yahoo.com>
2022-08-13 16:30:22 +0300

    navbar

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -8,7 +8,7 @@
     @foreach ( $sliders as $key => $slider)
     <div class="carousel-inner">
         <div class="carousel-item {{ $key == 0 ? 'active' : '' }}">
-          <img src="{{ asset("$slider->image") }}" class="d-block w-100 " >
+          <img src="{{ asset("$slider->image") }}" class="d-block w-100 " height=400px>
           
           <div class="carousel-caption d-none d-md-block">
             <div class="custom-carousel-content">
